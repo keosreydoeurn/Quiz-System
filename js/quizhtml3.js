5a5e5308664b0afbdc9e0bf0d9b1098aaeecd224
LEKSINAT <sinat.lek@student.passerellesnumeriques.org>
2026-01-02 10:49:30 +0700

    fix: mn of quizhtml3

--- js/quizhtml3.js
+++ js/quizhtml3.js
@@ -1,5 +1,5 @@
 // ================= QUIZ TIMER =================
-let totalTime = 15 * 60; // 15 minutes
+let totalTime = 5 * 60; // 15 minutes
 let timerInterval;
 
 window.addEventListener("DOMContentLoaded", () => {
